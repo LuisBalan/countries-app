0212a0f5fbbb079e17a13b2e28ba80145626baed
LuisBalan <jlbalcz@gmail.com>
2022-09-11 23:10:32 -0500

    create country-table component

--- src/app/country/country.module.ts
+++ src/app/country/country.module.ts
@@ -6,6 +6,7 @@ import { ByRegionComponent } from './pages/by-region/by-region.component';
 import { SeeCountryComponent } from './pages/see-country/see-country.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { CountryTableComponent } from './components/country-table/country-table.component';
 
 
 
@@ -14,7 +15,8 @@ import { RouterModule } from '@angular/router';
     ByCapitalComponent,
     ByCountryComponent,
     ByRegionComponent,
-    SeeCountryComponent
+    SeeCountryComponent,
+    CountryTableComponent
   ],
   exports: [
     ByCapitalComponent,
